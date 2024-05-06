dc0ce0be98dec4850d730a768937eba5135cb49e
aitorz09 <pqpiacarreras@gmail.com>
2024-05-06 21:09:41 +0200

    Deploy fix

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/RestCountryApi'>
       <App/>
     </BrowserRouter>
   </React.StrictMode>,
